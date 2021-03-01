df88915066d44aa0267d284536b5bfc6d150ccd4
Shiv <shivgunter@gmail.com>
2021-03-01 13:59:37 +0800

    rename

--- 0_scraper.py
+++ 0_scraper.py
@@ -11,19 +11,9 @@ pp = pprint.PrettyPrinter()
 # this gets some details about the app
 # result = app(PACKAGE, LANG, COUNTRY)
 
-""" Done already
+
 reviews = reviews_all(PACKAGE, lang=LANG, country=COUNTRY, sort=Sort.NEWEST)
 
-with open('otter_reviews','wb') as file:
+with open('otter_reviews_scraped','wb') as file:
     pickle.dump(reviews, file)
-"""
-
-# read with this:
-with open('otter_reviews', 'rb') as file:
-    reviews = pickle.load(file)
-
-reviews_by_stars = defaultdict(list)
-
-for review in reviews:
-    reviews_by_stars[review['score']].append(review)
 
